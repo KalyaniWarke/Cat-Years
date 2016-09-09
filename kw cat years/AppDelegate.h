c547e54b85c08ec154a931f7fea65453e4e78e98
Student P_04 <studentp_04@student-p-04.local>
2016-09-09 13:52:58 +0530

    Initial Commit

---
+++ kw cat years/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kw cat years
+//
+//  Created by Student P_04 on 09/09/16.
+//  Copyright © 2016 warke kalyani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kw cat years/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kw cat years
+//
+//  Created by Student P_04 on 09/09/16.
+//  Copyright © 2016 warke kalyani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
