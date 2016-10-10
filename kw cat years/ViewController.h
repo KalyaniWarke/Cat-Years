4d87375286974f2a6cf87370980194cdaceb1601
Kalyani <kalyani.warke18@gmail.com>
2016-10-10 14:25:15 +0530

    Added Files

--- kw cat years/ViewController.h
+++ kw cat years/ViewController.h
@@ -5,11 +5,22 @@
 //  Created by Student P_04 on 09/09/16.
 //  Copyright © 2016 warke kalyani. All rights reserved.
 //
-
+#define kAllUIElementHeight 60.0
+#define kHorizentalPadding 20.0
+#define kVerticalPadding 50.0
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
-
+@interface ViewController :UIViewController <UITextFieldDelegate>
+{
+    CGFloat screenWidth;
+    CGFloat screenHeight;
+    CGFloat humanAgeWidth;
+    CGFloat catButtonwidth;
+    CGFloat ageInCatYearsWidth;
+    CGFloat catButtonYCoordinate;
+    CGFloat ageInCatYearsYCoordinate;
+    UITextField *myHumanAge;
+    UILabel *ageInCatYears;
+}
 @end
 
--- kw cat years/ViewController.m
+++ kw cat years/ViewController.m
@@ -17,11 +17,102 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    screenWidth =[[UIScreen mainScreen]bounds].size.width;
+    screenHeight =[[UIScreen mainScreen]bounds].size.height;
+    humanAgeWidth = screenWidth -(2*kHorizentalPadding);
+    
+    catButtonYCoordinate = 100+kVerticalPadding +kAllUIElementHeight;
+    
+    ageInCatYearsYCoordinate = catButtonYCoordinate + kAllUIElementHeight+
+    (kVerticalPadding);
+    
+    ageInCatYearsWidth = screenWidth-(2*kHorizentalPadding);
+    
+    catButtonwidth = screenWidth -(8*kHorizentalPadding);
+    
+    self.view.backgroundColor=[UIColor grayColor];
+    
+    myHumanAge = [[UITextField alloc]initWithFrame:CGRectMake(kHorizentalPadding, 100, humanAgeWidth, kAllUIElementHeight)];
+   // [myHumanAge setText:@"Enter Human Age"];
+    
+    myHumanAge.backgroundColor = [UIColor whiteColor];
+    [myHumanAge setBorderStyle:UITextBorderStyleBezel];
+    myHumanAge.textAlignment = NSTextAlignmentCenter;
+    myHumanAge.delegate = self;
+    
+    myHumanAge.keyboardType = UIKeyboardTypeNumberPad;
+    
+    
+    
+    [self.view addSubview:myHumanAge];
+    UIButton *catButton =[[UIButton alloc]initWithFrame:CGRectMake((4*kHorizentalPadding), catButtonYCoordinate, catButtonwidth-(kHorizentalPadding), kAllUIElementHeight)];
+    
+    catButton.backgroundColor = [UIColor cyanColor];
+    
+    catButton.layer.cornerRadius = 20;
+    
+    [catButton addTarget:self action:@selector(handleCat) forControlEvents:UIControlEventTouchUpInside];
+    [catButton setTitle:@"Calculate Cat Year" forState:(UIControlStateNormal)];
+    [catButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:catButton];
+    
+    ageInCatYears =[[UILabel alloc]initWithFrame:CGRectMake(kHorizentalPadding, ageInCatYearsYCoordinate, ageInCatYearsWidth, kAllUIElementHeight)];
+    ageInCatYears.backgroundColor =[UIColor whiteColor];
+    ageInCatYears.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:ageInCatYears];
+    
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(void)handleCat{
+    
+    [self displayContent:myHumanAge.text];
+    
+    
+}
+
+-(void) displayContent:(NSString *)content
+{
+    int intAge = content.intValue;
+    if(content)
+    {
+        if(intAge>0&&intAge<115) {
+            NSString *currentValueString = myHumanAge.text;
+            
+            float currentValueInt = currentValueString.floatValue;
+            
+            currentValueInt = currentValueInt /7;
+            
+            NSString *newValueString = [NSString stringWithFormat:@"  Catyear is:%0.02f ",currentValueInt];
+            
+            ageInCatYears.text = newValueString;
+            
+        }
+        else {
+            ageInCatYears.text=@"InValid Age";
+        }
+        
+    }
+    else {
+        ageInCatYears.text=@"Please Enter the Age";
+        
+    }
+}
+
+    -(BOOL)textFieldShouldReturn:(UITextField *)UITextField
+{
+    
+    [myHumanAge resignFirstResponder];
+    
+    return YES;
+}
+-(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    NSLog(@"shake");
 }
 
 @end
